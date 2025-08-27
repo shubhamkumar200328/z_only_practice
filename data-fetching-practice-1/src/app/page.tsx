a47a38ab8fe05ca44febac5fc0d660c302b2c8ac
Shubham Kumar <shubhamkumar200328@gmail.com>
2025-08-28 00:44:15 +0530

    just started

---
+++ data-fetching-practice-1/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <div className=" my-10 m-auto text-center">
+        <h1 className=" text-2xl">Welcome Home!</h1>
+        <p className=" max-w-lg rounded-2xl m-auto my-11 text-3xl bg-cyan-950 p-11">Data Fetching Practice App_1</p>
+      </div>
+    </>
+  );
+}
